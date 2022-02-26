537bb057237f78988a0550dc5044277b859e1b30
Julien Panis <julienpanis@hotmail.com>
2022-02-26 15:23:26 +0100

    LPS25HB : read chip id
    Signed-off-by: Julien Panis <julienpanis@hotmail.com>

--- app/drivers/zephyr/sensor/my_sensor/my_sensor.c
+++ app/drivers/zephyr/sensor/my_sensor/my_sensor.c
@@ -42,21 +42,40 @@ static const struct sensor_driver_api my_sensor_api_funcs = {
 /* -------------------- my_sensor_init -------------------- */
 static int my_sensor_init(const struct device *dev)
 {
+	const struct my_sensor_config * const config = dev->config;
+	struct my_sensor_data *data = dev->data;
+	uint8_t chip_id = 0;
 	
-
+	// Get I2C master device
+	data->i2c_master = device_get_binding(config->i2c_master_dev_name);
+	if (!data->i2c_master) {
+		LOG_DBG("i2c master not found: %s", config->i2c_master_dev_name);
+		return -EINVAL;
+	}
+	
+	// Check chip ID
+	if (i2c_reg_read_byte(data->i2c_master, config->i2c_slave_addr, MY_SENSOR_REG_WHO_AM_I, &chip_id) < 0) {
+		LOG_DBG("failed reading chip id");
+		return -EIO;
+	}
+	
+	if (chip_id != MY_SENSOR_VAL_WHO_AM_I) {
+		LOG_DBG("invalid chip id 0x%x", chip_id);
+		return -EIO;
+	}
+	
 	return 0;
 }
 
 
 /* -------------------- struct -------------------- */
 static const struct my_sensor_config my_sensor_config = {
-	.dummy = 0
+	.i2c_master_dev_name = DT_INST_BUS_LABEL(0),
+	.i2c_slave_addr = DT_INST_REG_ADDR(0),
 };
 
 
-static struct my_sensor_data my_sensor_data = {
-	.dummy = 0
-};
+static struct my_sensor_data my_sensor_data;
 
 
 DEVICE_DT_INST_DEFINE(0, my_sensor_init, NULL,
--- app/drivers/zephyr/sensor/my_sensor/my_sensor.h
+++ app/drivers/zephyr/sensor/my_sensor/my_sensor.h
@@ -2,14 +2,39 @@
 #define _MY_SENSOR_H_
 
 
+#include <drivers/i2c.h>
+
+
+/* -------------------- registers -------------------- */
+#define MY_SENSOR_REG_WHO_AM_I     0x0f
+#define MY_SENSOR_VAL_WHO_AM_I     0xbd
+
+#define MY_SENSOR_REG_CTRL_REG1    0x20 //< Power up-down + Output data rate
+#define MY_SENSOR_REG_CTRL_REG2    0x21 //< FIFO mode + Watermark mode
+#define MY_SENSOR_REG_CTRL_REG3    0x22 //< INT1 active low
+#define MY_SENSOR_REG_CTRL_REG4    0x23 //< Watermark signal on INT1 pin
+
+#define MY_SENSOR_REG_INT_SOURCE   0x25 //< INT1 acknowledgment
+
+#define MY_SENSOR_REG_PRESS_OUT_XL 0x28 //< Pressure low
+#define MY_SENSOR_REG_PRESS_OUT_L  0x29 //< Pressure medium
+#define MY_SENSOR_REG_PRESS_OUT_H  0x2a //< Pressure high
+
+#define MY_SENSOR_REG_FIFO_CTRL    0x2e //< Stream-Bypass mode (FIFO) + Watermark level
+
+
+/* -------------------- struct -------------------- */
 struct my_sensor_config {
-	uint8_t dummy;
+	char *i2c_master_dev_name;
+	uint16_t i2c_slave_addr;
 };
 
 
 struct my_sensor_data {
-	uint8_t dummy;
+	const struct device *i2c_master;
+	int32_t sample_press;
 };
 
 
 #endif /* _MY_SENSOR_H_ */
+
