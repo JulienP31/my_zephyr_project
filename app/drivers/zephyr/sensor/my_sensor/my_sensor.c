4b90c430f88e1b34aee07f938077151b58a9768c
Julien Panis <julienpanis@hotmail.com>
2022-02-27 18:20:03 +0100

    LPS25HB : implemented API (without IRQ)
    Signed-off-by: Julien Panis <julienpanis@hotmail.com>

--- app/drivers/zephyr/sensor/my_sensor/my_sensor.c
+++ app/drivers/zephyr/sensor/my_sensor/my_sensor.c
@@ -14,8 +14,19 @@ LOG_MODULE_REGISTER(MY_SENSOR, CONFIG_SENSOR_LOG_LEVEL);
 static int my_sensor_sample_fetch(const struct device *dev,
 				enum sensor_channel chan)
 {
+	struct my_sensor_data *data = dev->data;
+	const struct my_sensor_config *config = dev->config;
+	uint8_t out[MY_SENSOR_PRESS_DATA_SIZE] = {0};
 	
-
+	__ASSERT_NO_MSG(chan == SENSOR_CHAN_PRESS);
+	
+	if (i2c_burst_read(data->i2c_master, config->i2c_slave_addr, MY_SENSOR_REG_PRESS_OUT, out, MY_SENSOR_PRESS_DATA_SIZE) < 0) {
+		LOG_DBG("failed to read sample");
+		return -EIO;
+	}
+	
+	data->sample_press = (int32_t)(  ((uint32_t)(out[2]) << 16) | ((uint32_t)(out[1]) << 8) | out[0]  );
+	
 	return 0;
 }
 
@@ -25,9 +36,13 @@ static int my_sensor_channel_get(const struct device *dev,
 			       enum sensor_channel chan,
 			       struct sensor_value *val)
 {
-	val->val1 = 123;
-	val->val2 = 456789;
-
+	struct my_sensor_data *data = dev->data;
+	
+	__ASSERT_NO_MSG(chan == SENSOR_CHAN_PRESS);
+	
+	val->val1 = data->sample_press / 40960;
+	val->val2 = ((int32_t)data->sample_press * 1000000 / 40960) % 1000000;
+	
 	return 0;
 }
 
@@ -37,6 +52,54 @@ static const struct sensor_driver_api my_sensor_api_funcs = {
 	.sample_fetch = my_sensor_sample_fetch,
 	.channel_get = my_sensor_channel_get,
 };
+
+
+/* -------------------- my_sensor_poweroff -------------------- */
+static void my_sensor_poweroff(const struct device *dev)
+{
+	struct my_sensor_data *data = dev->data;
+	const struct my_sensor_config *config = dev->config;
+	
+	if (i2c_reg_write_byte(data->i2c_master, config->i2c_slave_addr, MY_SENSOR_REG_CTRL_REG1, 0x00) < 0) {
+		LOG_DBG("failed to power off chip");
+	}
+}
+
+
+/* -------------------- my_sensor_configure -------------------- */
+static int my_sensor_configure(const struct device *dev)
+{
+	struct my_sensor_data *data = dev->data;
+	const struct my_sensor_config *config = dev->config;
+	uint8_t byte = 0;
+	
+	// Power up + Output data rate
+	byte = (1 << 7) | ( (MY_SENSOR_DEFAULT_SAMPLING_RATE & 0x07) << 4 );
+	if (i2c_reg_write_byte(data->i2c_master, config->i2c_slave_addr, MY_SENSOR_REG_CTRL_REG1, byte) < 0) {
+		LOG_DBG("failed to set ODR");
+		goto err_poweroff;
+	}
+#if 0
+	// INT_DRDY active low
+	byte = (1 << 7);
+	if (i2c_reg_write_byte(data->i2c_master, config->i2c_slave_addr, MY_SENSOR_REG_CTRL_REG3, byte) < 0) {
+		LOG_DBG("failed to set interrupt");
+		goto err_poweroff;
+	}
+	
+	// DRDY signal on INT_DRDY pin
+	byte = 0x01;
+	if (i2c_reg_write_byte(data->i2c_master, config->i2c_slave_addr, MY_SENSOR_REG_CTRL_REG4, byte) < 0) {
+		LOG_DBG("failed to route interrupt");
+		goto err_poweroff;
+	}
+#endif
+	return 0;
+
+err_poweroff:
+	my_sensor_poweroff(dev);
+	return -EIO;
+}
 
 
 /* -------------------- my_sensor_init -------------------- */
@@ -55,12 +118,18 @@ static int my_sensor_init(const struct device *dev)
 	
 	// Check chip ID
 	if (i2c_reg_read_byte(data->i2c_master, config->i2c_slave_addr, MY_SENSOR_REG_WHO_AM_I, &chip_id) < 0) {
-		LOG_DBG("failed reading chip id");
+		LOG_DBG("failed to read chip id");
 		return -EIO;
 	}
 	
 	if (chip_id != MY_SENSOR_VAL_WHO_AM_I) {
 		LOG_DBG("invalid chip id 0x%x", chip_id);
+		return -EIO;
+	}
+	
+	// Configure chip
+	if (my_sensor_configure(dev) < 0) {
+		LOG_DBG("failed to configure chip");
 		return -EIO;
 	}
 	
--- app/drivers/zephyr/sensor/my_sensor/my_sensor.h
+++ app/drivers/zephyr/sensor/my_sensor/my_sensor.h
@@ -10,17 +10,28 @@
 #define MY_SENSOR_VAL_WHO_AM_I     0xbd
 
 #define MY_SENSOR_REG_CTRL_REG1    0x20 //< Power up-down + Output data rate
-#define MY_SENSOR_REG_CTRL_REG2    0x21 //< FIFO mode + Watermark mode
-#define MY_SENSOR_REG_CTRL_REG3    0x22 //< INT1 active low
-#define MY_SENSOR_REG_CTRL_REG4    0x23 //< Watermark signal on INT1 pin
+#define MY_SENSOR_REG_CTRL_REG3    0x22 //< INT_DRDY active low
+#define MY_SENSOR_REG_CTRL_REG4    0x23 //< DRDY signal on INT_DRDY pin
 
-#define MY_SENSOR_REG_INT_SOURCE   0x25 //< INT1 acknowledgment
+#define MY_SENSOR_REG_INT_SOURCE   0x25 //< INT_DRDY acknowledgment
 
 #define MY_SENSOR_REG_PRESS_OUT_XL 0x28 //< Pressure low
 #define MY_SENSOR_REG_PRESS_OUT_L  0x29 //< Pressure medium
 #define MY_SENSOR_REG_PRESS_OUT_H  0x2a //< Pressure high
+#define MY_SENSOR_REG_PRESS_OUT    (MY_SENSOR_REG_PRESS_OUT_XL | 0x80) //< MSB for address auto-increment
 
-#define MY_SENSOR_REG_FIFO_CTRL    0x2e //< Stream-Bypass mode (FIFO) + Watermark level
+#define MY_SENSOR_PRESS_DATA_SIZE  3
+
+
+#if CONFIG_MY_SENSOR_SAMPLING_RATE == 1
+	#define MY_SENSOR_DEFAULT_SAMPLING_RATE 1
+#elif CONFIG_MY_SENSOR_SAMPLING_RATE == 7
+	#define MY_SENSOR_DEFAULT_SAMPLING_RATE 2
+#elif CONFIG_MY_SENSOR_SAMPLING_RATE == 13
+	#define MY_SENSOR_DEFAULT_SAMPLING_RATE 3
+#elif CONFIG_MY_SENSOR_SAMPLING_RATE == 25
+	#define MY_SENSOR_DEFAULT_SAMPLING_RATE 4
+#endif
 
 
 /* -------------------- struct -------------------- */
